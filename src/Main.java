681e17cd1a06080902959722aada1310d074157f
Marta <s34922@pjwstk.edu.pl>
2026-03-19 19:38:48 +0100

    Dodanie pomocniczej informacji o aplikacji

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Moja pierwsza praca z Gitem!")
+        System.out.println("Moja pierwsza praca z Gitem!!! :)")
 
     }
 }
